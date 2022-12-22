5fe145b080cdbcb9cdeed4a9ccfae135713ba950
Alex Wu <alexwu727@gmail.com>
2022-12-22 09:40:43 -0800

    use state in App

--- src/App.js
+++ src/App.js
@@ -13,6 +13,11 @@ const btnValues = [
 ];
 
 const App = () => {
+  const [calc, setCalc] = useState({
+    sign: "",
+    num: 0,
+    res: 0,
+  });
   return (
     <Wrapper>
       <Screen value={calc.num ? calc.num : calc.res} />
